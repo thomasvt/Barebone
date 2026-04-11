e1737e2a1da392e021f86176bcde19b4780d616c
Thomas Vantroyen <thomas.vantroyen@gmail.com>
2026-04-11 17:04:26 +0200

    BBPOC: debug features: break and alter GameSpeed

---
+++ Barebone.Game/Debugging/DebugSubSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using Barebone.Game.Input;
+
+namespace Barebone.Game.Debug
+{
+    internal class DebugSubSystem(Engine engine) : IDebug
+    {
+        public void Update(IBBApi bb)
+        {
+            if (bb.Input.JustPressed(KeyboardKey.Grave)) WriteDebugScreen();
+            if (bb.Input.JustPressed(KeyboardKey.NumPadPlus)) { IncreaseGameSpeed(); WriteDebugScreen(); } 
+            if (bb.Input.JustPressed(KeyboardKey.NumPadMinus)) { DecreaseGameSpeed(); WriteDebugScreen(); }
+            if (bb.Input.JustPressed(KeyboardKey.NumPadEnter)) { WriteDebugScreen(); Debugger.Break(); }
+        }
+        
+        public void IncreaseGameSpeed()
+        {
+            engine.Speed *= 2f;
+            
+        }
+
+        public void DecreaseGameSpeed()
+        {
+            engine.Speed *= 0.5f;
+        }
+
+        public float GameSpeed => engine.Speed;
+
+        private void WriteDebugScreen()
+        {
+            Clear();
+            WriteLine("BareBoneGame - DEBUG MENU");
+            WriteLine("-------------------------");
+            WriteLine($"[+] [-]  Speed: {engine.Speed*100:0}%");
+            WriteLine( "[Enter]  Debug break");
+            WriteLine();
+        }
+
+        public void Clear()
+        {
+            Console.Clear();
+        }
+
+        public void WriteLine(object? msg = null)
+        {
+            Console.WriteLine(msg?.ToString());
+        }
+
+        public void Write(object msg)
+        {
+            Console.Write(msg.ToString());
+        }
+    }
+}
---
+++ Barebone.Game/Debugging/IDebug.cs
@@ -0,0 +1,12 @@
+﻿namespace Barebone.Game.Debug
+{
+    public interface IDebug
+    {
+        void IncreaseGameSpeed();
+        void DecreaseGameSpeed();
+        void Clear();
+        void WriteLine(object? msg = null);
+        void Write(object msg);
+        float GameSpeed { get; }
+    }
+}
